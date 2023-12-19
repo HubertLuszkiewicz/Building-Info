2b46eaa0a1a3774d87f257d8fa6c23288c5e295e
lkucharczyk <lkucharczyk@protonmail.com>
2023-12-19 05:23:13 +0100

    Add javadoc CI + 2 more documented classes

--- src/main/java/pl/put/poznan/buildinginfo/models/LocationContainer.java
+++ src/main/java/pl/put/poznan/buildinginfo/models/LocationContainer.java
@@ -2,36 +2,72 @@ package pl.put.poznan.buildinginfo.models;
 
 import java.util.List;
 
+/**
+ * Generic class representing a location that can hold sublocations,
+ * that holds no other information on its own. (i.e. Building contains Floors)
+ * @param <Sublocation> Sublocation type
+ */
 public class LocationContainer<Sublocation extends Location> extends Location{
-
+    /**
+     * Default constructor
+     * @param id Location ID
+     * @param name Location name
+     * @param type Location type (unused)
+     * @param locations Sublocations list
+     */
     public LocationContainer(Integer id, String name, Types type, List<Sublocation> locations) {
         super(id, name);
         this.locations = locations;
     }
 
-    private List<Sublocation> locations;
+    /** Container holding sublocation references */
+    private final List<Sublocation> locations;
 
+    /**
+     * Add a sublocation to the container
+     * @param location Sublocation to be added
+     */
     public void addLocation(Sublocation location) {
         locations.add(location);
     }
 
+    /**
+     * Remove location from the container
+     * @param location Sublocation to be removed
+     */
     public void removeLocation(Sublocation location) {
         locations.remove(location);
     }
 
+    /**
+     * Get all direct sublocations of this container
+     * @return Sublocation list
+     */
     public List<Sublocation> getLocations() {
         return locations;
     }
 
+    /**
+     * @return Area sum of its sublocations
+     */
     @Override
     public Float getArea() { return this.getLocations().stream().map(Sublocation::getArea).reduce(0f, Float::sum); }
 
+    /**
+     * @return Volume sum of its sublocations
+     */
     @Override
     public Float getVolume() { return this.getLocations().stream().map(Sublocation::getVolume).reduce(0f, Float::sum); }
 
+    /**
+     * @return Total heating power usage in all sublocations
+     */
     @Override
     public Float getTotalHeating() { return this.getLocations().stream().map(Sublocation::getTotalHeating).reduce(0f, Float::sum); }
 
+    /**
+     * @return Total lighting intensity in all sublocations
+     */
     @Override
     public Integer getTotalLighting() { return this.getLocations().stream().map(Sublocation::getTotalLighting).reduce(0, Integer::sum); }
 }
--- src/main/java/pl/put/poznan/buildinginfo/models/Room.java
+++ src/main/java/pl/put/poznan/buildinginfo/models/Room.java
@@ -1,31 +1,62 @@
 package pl.put.poznan.buildinginfo.models;
 
+/**
+ * Class representing room in a building
+ */
 public class Room extends Location {
+    /**
+     * Room's area (use getArea() instead)
+     * @see Room#getArea()
+     */
     public Float area;
-    public Float cube;
+    /**
+     * Room's volume (use getVolume() instead)
+     * @see Room#getVolume()
+     */
+    public Float volume;
+    /**
+     * Room's heating power usage (use getTotalHeating() instead)
+     * @see Room#getTotalHeating()
+     */
     public Float heating;
+    /**
+     * Room's total lighting intensity (use getTotalLighting() instead)
+     * @see Room#getTotalLighting()
+     *
+     */
     public Integer lighting;
 
-    public Room(Integer id, String name, Float area, Float cube, Float heating, Integer lighting) {
+    /**
+     * Default constructor for initializing all properties
+     * @param id Location ID
+     * @param name Location name
+     * @param area Room's area
+     * @param volume Room's volume
+     * @param heating Room's heating power usage
+     * @param lighting Room's total lighting intensity
+     */
+    public Room(Integer id, String name, Float area, Float volume, Float heating, Integer lighting) {
         super(id, name);
         this.area = area;
         this.type = Types.ROOM;
-        this.cube = cube;
+        this.volume = volume;
         this.heating = heating;
         this.lighting = lighting;
     }
 
+    /** @return Room's area */
     @Override
-    public Float getArea() {
-        return this.area;
-    }
+    public Float getArea() { return this.area; }
 
+    /** @return Room's volume */
     @Override
-    public Float getVolume() { return this.cube; }
+    public Float getVolume() { return this.volume; }
 
+    /** @return Room's heating power usage */
     @Override
     public Float getTotalHeating() { return this.heating; }
 
+    /** @return Room's total lighting intensity */
     @Override
     public Integer getTotalLighting() { return this.lighting; }
 }
